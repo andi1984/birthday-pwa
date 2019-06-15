cd9d46e6ae01db2bf0eebf955478e237b6afee42
Andreas Sander <andreas.sander@hotmail.de>
2019-06-15 23:17:11 +0200

    Image preview

--- components/Event.js
+++ components/Event.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Countdown from 'react-countdown-now'
 import Calendar from 'rc-calendar'
+import Dropzone from 'react-dropzone'
 import germanLocale from 'rc-calendar/lib/locale/de_DE'
 import 'rc-calendar/assets/index.css'
 import moment from 'moment'
 
 const Event = ({ title, date }) => {
     const dateInMomentFormat = moment(date)
+    const [image, setImage] = useState(null)
     return (
         <Fragment>
             <h1>{title}</h1>
@@ -48,6 +50,39 @@ const Event = ({ title, date }) => {
                     }
                 }}
             />
+            <Dropzone
+                onDrop={acceptedFiles => {
+                    const reader = new FileReader()
+
+                    reader.onabort = () =>
+                        console.log('file reading was aborted')
+                    reader.onerror = () =>
+                        console.log('file reading has failed')
+                    reader.onload = () => {
+                        // Do whatever you want with the file contents
+                        const arrayBuffer = reader.result
+                        const blob = new Blob([new Uint8Array(arrayBuffer)])
+                        setImage(blob)
+                    }
+
+                    acceptedFiles.forEach(file =>
+                        reader.readAsArrayBuffer(file)
+                    )
+                }}
+            >
+                {({ getRootProps, getInputProps }) => (
+                    <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>
+                                Drag 'n' drop some files here, or click to
+                                select files
+                            </p>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
+            {!!image && <img src={URL.createObjectURL(image)} />}
         </Fragment>
     )
 }
