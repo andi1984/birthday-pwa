b5520e639d841b3a28d770ffda9d42952e0d7431
Andreas Sander <andreas.sander@hotmail.de>
2019-06-15 14:22:21 +0200

    Add sourcemaps

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-    cssLoaderOptions: {
-        url: true,
-    },
-    webpack: config => {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-            fs: 'empty',
-        }
+const withSourceMaps = require('@zeit/next-source-maps')()
+module.exports = withCSS(
+    withSourceMaps({
+        cssLoaderOptions: {
+            url: true,
+        },
+        webpack: config => {
+            // Fixes npm packages that depend on `fs` module
+            config.node = {
+                fs: 'empty',
+            }
 
-        return config
-    },
-})
+            return config
+        },
+    })
+)
