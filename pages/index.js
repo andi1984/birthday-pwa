dcf6cea27e2eeb37e86a10d3ec8d94754f874d7f
Andreas Sander <andreas.sander@hotmail.de>
2019-06-15 14:46:57 +0200

    Use effect and state to get event data.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { withRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import queryString from 'query-string'
 import Event from '../components/Event'
 import { getEventDataByEventQueryParam } from '../helper/event'
 import eventData from '../data/data.json'
-const Home = ({ router }) => {
-    let eventID, event
-    if ('id' in router.query) {
-        eventID = router.query.id
-        event = getEventDataByEventQueryParam(eventID, eventData)
-    }
+const Home = () => {
+    const [event, setEvent] = useState(null)
+    useEffect(() => {
+        const query = queryString.parse(window.location.search)
+        if ('id' in query) {
+            setEvent(getEventDataByEventQueryParam(query.id, eventData))
+        }
+    }, [])
 
     return (
         <main>{!!event ? <Event {...event} /> : <p>Ungültiger Link</p>}</main>
     )
 }
 
-export default withRouter(Home)
+export default Home
